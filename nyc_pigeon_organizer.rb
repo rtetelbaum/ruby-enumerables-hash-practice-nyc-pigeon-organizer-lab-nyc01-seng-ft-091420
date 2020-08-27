b8c63b89e12c9b179b78093a323e594353d25ef1
Roman Tetelbaum <rtetelbaum@mailbox.org>
2020-08-27 05:07:33 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,15 +1,15 @@
 def nyc_pigeon_organizer(data)
   pigeons = {}
   data.each do |key, value|
-    value.each do |inner_value, names|
+    value.each do |inner_key, names|
       names.each do |name|
         if !pigeons[name]
           pigeons[name] = {}
         end
         if !pigeons[name][key]
-          pigeons[name][key] = []
+          !pigeons[name][key] = []
         end
-        pigeons[name][key].push(inner_value.to_s)
+        pigeons[name][key].push(inner_key.to_s)
       end
     end
   end
